b93093b00953473412976ea2c1f8140754da4734
toyama <tyo19990215@gmail.com>
2025-05-16 16:03:48 +0900

    Resolved merge conflicts

---
+++ googletest/googletest-1.15.2/build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/DxLib_VC/DX_LibData/tyoNaoki/entityProject/googletest/googletest-1.15.2/googletest
+# Build directory: D:/DxLib_VC/DX_LibData/tyoNaoki/entityProject/googletest/googletest-1.15.2/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
