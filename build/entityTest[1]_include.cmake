09c2526d6304658928c3d392c62d63a5c1088d51
tyoNaoki <47668134+tyoNaoki@users.noreply.github.com>
2025-06-14 14:28:35 +0900

    Delete build directory

--- build/entityTest[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "D:/DxLib_VC/DX_LibData/tyoNaoki/entityProject/build/entityTest[1]_tests.cmake")
-  include("D:/DxLib_VC/DX_LibData/tyoNaoki/entityProject/build/entityTest[1]_tests.cmake")
-else()
-  add_test(entityTest_NOT_BUILT entityTest_NOT_BUILT)
-endif()
