b7d8ab777229acb33947243bf3e5679d19559aa4
tyoNaoki <tyo19990215@gmail.com>
2026-05-11 03:10:10 +0900

    Cube並列処理用のデモ作成

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,5 +1,5 @@
 #include "DxLib.h"
-#include "ecsTest.h"
+#include "parallelCubeDEMO.h"
 #include <iostream>
 #include <windows.h>
 
@@ -14,7 +14,13 @@ void viewConsole()
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); //ウィンドウモード変更と初期化と裏画面設定
+	ChangeWindowMode(TRUE);
+	SetGraphMode(1280, 720, 32);
+	DxLib_Init();
+
+	SetDrawScreen(DX_SCREEN_BACK); //ウィンドウモード変更と初期化と裏画面設定
+	
+	
 
 	viewConsole();
 
@@ -22,13 +28,27 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	int Green = GetColor(0, 255, 0);      // 緑の色コードを取得
 
 	bool isNotFinish = false;
+
+	
+
+	int64_t prevTime = GetNowHiPerformanceCount();
 	
 	// while(裏画面を表画面に反映, メッセージ処理, 画面クリア)
-	while (!isNotFinish && ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0)
+	while (!isNotFinish && ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0 && CheckHitKey(KEY_INPUT_RETURN) == 0)
 	{
-		ecs_Test();
-		//Transform2D trans(150,100);
-		//DrawFormatString(0,0, Green, "座標[%d,%d]", 0,0); // 文字を描画する
+		int64_t now = GetNowHiPerformanceCount();
+		float deltaTime = (now - prevTime) / 1000000.0f; // 秒
+
+		cube_demo(deltaTime);
+		
+		prevTime = now;
+
+		/*for (CubeComponent& cube : cubes)
+		{
+			cube.transform.rotation.y += 0.02f;
+			UpdateTransformSystem(cube);
+			DrawCubeSystem(cube);
+		}*/
 	}
 
 	DxLib_End();				// ＤＸライブラリ使用の終了処理
